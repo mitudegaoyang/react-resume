b3b0489a3533644de7a5b2a1c1b53ec7b84ee71a
迷途的羔羊 <13683265113@163.com>
2023-10-16 15:58:30 +0800

    feat: 测试build

---
+++ docs/precache-manifest.eed3f0ccecdef0af5f2774d4f6323e86.js
@@ -0,0 +1,270 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cc93dc01f34bd4ab03c9a854b049f2c",
+    "url": "/react-resume/index.html"
+  },
+  {
+    "revision": "3ec722ccc3983103e5fd",
+    "url": "/react-resume/static/css/0.ae59248a.chunk.css"
+  },
+  {
+    "revision": "90d6402dc7dd92cebd8b",
+    "url": "/react-resume/static/css/1.e940edd6.chunk.css"
+  },
+  {
+    "revision": "15679abe76ba8ea22eaf",
+    "url": "/react-resume/static/css/3.5821fad2.chunk.css"
+  },
+  {
+    "revision": "9c4824635202d5280c83",
+    "url": "/react-resume/static/css/33.7cb6dce6.chunk.css"
+  },
+  {
+    "revision": "d2684094c9ad3a484f30",
+    "url": "/react-resume/static/css/34.76b253d2.chunk.css"
+  },
+  {
+    "revision": "a32b0be4a010dcf3dd6d",
+    "url": "/react-resume/static/css/35.9b6bfd1c.chunk.css"
+  },
+  {
+    "revision": "4995961dfe650c8510fc",
+    "url": "/react-resume/static/css/36.e92ed2f2.chunk.css"
+  },
+  {
+    "revision": "505ec12e9cca69889771",
+    "url": "/react-resume/static/css/39.f824c340.chunk.css"
+  },
+  {
+    "revision": "b05194a7873315a83169",
+    "url": "/react-resume/static/css/5.915e11f9.chunk.css"
+  },
+  {
+    "revision": "451a820bf69c4dcbc35f",
+    "url": "/react-resume/static/css/Dashboard.226770ab.chunk.css"
+  },
+  {
+    "revision": "8cc04f0f3fc543e41d62",
+    "url": "/react-resume/static/css/Error404.b27f81ed.chunk.css"
+  },
+  {
+    "revision": "3d82aa82c767567555bd",
+    "url": "/react-resume/static/css/RichTextEditor.bae160b2.chunk.css"
+  },
+  {
+    "revision": "115652257d49d59ae58d",
+    "url": "/react-resume/static/css/UploadExcel.9579c327.chunk.css"
+  },
+  {
+    "revision": "57cc6c7e93ada48362f9",
+    "url": "/react-resume/static/css/main.57b4bd6c.chunk.css"
+  },
+  {
+    "revision": "3ec722ccc3983103e5fd",
+    "url": "/react-resume/static/js/0.ed3a4eab.chunk.js"
+  },
+  {
+    "revision": "90d6402dc7dd92cebd8b",
+    "url": "/react-resume/static/js/1.395e5fbd.chunk.js"
+  },
+  {
+    "revision": "b9955ff7815fca29eb2a",
+    "url": "/react-resume/static/js/2.cd6576f9.chunk.js"
+  },
+  {
+    "revision": "15679abe76ba8ea22eaf",
+    "url": "/react-resume/static/js/3.51e25788.chunk.js"
+  },
+  {
+    "revision": "9c4824635202d5280c83",
+    "url": "/react-resume/static/js/33.02b74722.chunk.js"
+  },
+  {
+    "revision": "d2b90ea09d1da78c9e4151c55ab06370",
+    "url": "/react-resume/static/js/33.02b74722.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2684094c9ad3a484f30",
+    "url": "/react-resume/static/js/34.8b92ace2.chunk.js"
+  },
+  {
+    "revision": "d5df3d82ef6a86f73f7a708da6bee9b5",
+    "url": "/react-resume/static/js/34.8b92ace2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a32b0be4a010dcf3dd6d",
+    "url": "/react-resume/static/js/35.81e45b36.chunk.js"
+  },
+  {
+    "revision": "4995961dfe650c8510fc",
+    "url": "/react-resume/static/js/36.16e9d674.chunk.js"
+  },
+  {
+    "revision": "0704dda96154134246a761973e52c0f9",
+    "url": "/react-resume/static/js/36.16e9d674.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60c7f94dd2b556909223",
+    "url": "/react-resume/static/js/37.a3f7df16.chunk.js"
+  },
+  {
+    "revision": "74553ab6d81da945cc19",
+    "url": "/react-resume/static/js/38.79158b1a.chunk.js"
+  },
+  {
+    "revision": "505ec12e9cca69889771",
+    "url": "/react-resume/static/js/39.22f746c6.chunk.js"
+  },
+  {
+    "revision": "3c59e861f4473d751dcc",
+    "url": "/react-resume/static/js/4.4ab72d42.chunk.js"
+  },
+  {
+    "revision": "c47fb89f944fc413937f1d857df6495a",
+    "url": "/react-resume/static/js/4.4ab72d42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "297b031f6dfcf05c29f4",
+    "url": "/react-resume/static/js/40.3d9283ce.chunk.js"
+  },
+  {
+    "revision": "b05194a7873315a83169",
+    "url": "/react-resume/static/js/5.a8c7ae06.chunk.js"
+  },
+  {
+    "revision": "667371d71312c64f9f6d",
+    "url": "/react-resume/static/js/6.9939a786.chunk.js"
+  },
+  {
+    "revision": "008bda0c714913ac9ada",
+    "url": "/react-resume/static/js/About.d4d38c6e.chunk.js"
+  },
+  {
+    "revision": "db70cfc28c685257469a",
+    "url": "/react-resume/static/js/AdminPage.71bd481d.chunk.js"
+  },
+  {
+    "revision": "03e72fb49861c08501b5",
+    "url": "/react-resume/static/js/Bug.9dc81e7e.chunk.js"
+  },
+  {
+    "revision": "1ccb8f5f6f68c4f3c272",
+    "url": "/react-resume/static/js/Clipboard.41a1f92f.chunk.js"
+  },
+  {
+    "revision": "451a820bf69c4dcbc35f",
+    "url": "/react-resume/static/js/Dashboard.91b06a1f.chunk.js"
+  },
+  {
+    "revision": "d9a502b7be8da4c7b8d7",
+    "url": "/react-resume/static/js/Doc.c199718b.chunk.js"
+  },
+  {
+    "revision": "e4a1fe815a5c9620124e",
+    "url": "/react-resume/static/js/Draggable.a902e403.chunk.js"
+  },
+  {
+    "revision": "e8fcac857060bc29e52a",
+    "url": "/react-resume/static/js/EditorPage.f66e2c39.chunk.js"
+  },
+  {
+    "revision": "8cc04f0f3fc543e41d62",
+    "url": "/react-resume/static/js/Error404.cbcc04ae.chunk.js"
+  },
+  {
+    "revision": "0c8c0ecbfac68cec03c7",
+    "url": "/react-resume/static/js/Explanation.44e38a4e.chunk.js"
+  },
+  {
+    "revision": "1924f48b2f422a18f5c9",
+    "url": "/react-resume/static/js/ExportExcel.76b1733c.chunk.js"
+  },
+  {
+    "revision": "4e58e89314b9594e6485",
+    "url": "/react-resume/static/js/GuestPage.92f4cf0b.chunk.js"
+  },
+  {
+    "revision": "e3ac94f9b086c136868c",
+    "url": "/react-resume/static/js/Guide.523fbc61.chunk.js"
+  },
+  {
+    "revision": "7fb30fa67ce426055a3f",
+    "url": "/react-resume/static/js/KeyboardChart.813b5446.chunk.js"
+  },
+  {
+    "revision": "48349b1f0c3855b6b4f6",
+    "url": "/react-resume/static/js/LineChart.bb497e90.chunk.js"
+  },
+  {
+    "revision": "9269a21f8e240380ade0",
+    "url": "/react-resume/static/js/Markdown.5b37da7b.chunk.js"
+  },
+  {
+    "revision": "2071e4b8e6d64bfb0531",
+    "url": "/react-resume/static/js/Menu1_1.1fe6b113.chunk.js"
+  },
+  {
+    "revision": "1476b1599277eadb2998",
+    "url": "/react-resume/static/js/Menu1_2_1.565f86aa.chunk.js"
+  },
+  {
+    "revision": "5c61f272e1aecc5f4dcd",
+    "url": "/react-resume/static/js/MixChart.4d78e176.chunk.js"
+  },
+  {
+    "revision": "3d82aa82c767567555bd",
+    "url": "/react-resume/static/js/RichTextEditor.b7c60ace.chunk.js"
+  },
+  {
+    "revision": "1b7ec67fad0dc7526a19",
+    "url": "/react-resume/static/js/Table.509d7b5a.chunk.js"
+  },
+  {
+    "revision": "115652257d49d59ae58d",
+    "url": "/react-resume/static/js/UploadExcel.aeb95e88.chunk.js"
+  },
+  {
+    "revision": "8366aec24d1793c678e0",
+    "url": "/react-resume/static/js/User.a9737e71.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/react-resume/static/js/User.a9737e71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6b9366dd843013557fc",
+    "url": "/react-resume/static/js/Zip.13b4c28f.chunk.js"
+  },
+  {
+    "revision": "57cc6c7e93ada48362f9",
+    "url": "/react-resume/static/js/main.c0f06df5.chunk.js"
+  },
+  {
+    "revision": "db79729b464f19d6bf1b",
+    "url": "/react-resume/static/js/runtime-main.b29249ca.js"
+  },
+  {
+    "revision": "9161e238b798c444b732dbbb7420a09e",
+    "url": "/react-resume/static/media/404.9161e238.png"
+  },
+  {
+    "revision": "2701e8c903c6ea62b30e36aae59060a1",
+    "url": "/react-resume/static/media/bg.2701e8c9.jpg"
+  },
+  {
+    "revision": "e58eaef0ee605eb27eb5117c9d10f189",
+    "url": "/react-resume/static/media/draggable.e58eaef0.gif"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-resume/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5a232d53fba12e27ea5b5c16e5b1acd4",
+    "url": "/react-resume/static/media/reward.5a232d53.jpg"
+  },
+  {
+    "revision": "2a5b6308244222a5b5335da0ebb9265b",
+    "url": "/react-resume/static/media/wechat.2a5b6308.jpg"
+  }
+]);
